91742934459aa439f5e5c80c549e5f2045d4dc56
joo-yongjae <0443jun@gmail.com>
2024-02-29 01:10:46 +0900

    first commit

---
+++ src/main/java/com/jooseoan/jooseoan_backend/JooseoanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jooseoan.jooseoan_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooseoanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JooseoanBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jooseoan/jooseoan_backend/JooseoanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jooseoan.jooseoan_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooseoanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
